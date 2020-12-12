cf9fec2b927be8f50143fbec278d0de099418da0
h552hgg <h552hgg@gmail.com>
2020-12-11 18:11:16 -0600

    Update files

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask
+
+
+#################################################
+# Database Setup
+#################################################
+# database_path = "/Users/jonathanrocha/Desktop/SQL Alchemy/Resources/hawaii.sqlite"
+# engine = create_engine(f"sqlite:///{database_path}")
+
+
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
+
+# # Save reference to the table
+# Hawaii = Base.classes.hawaii
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def home():
+    print("Server request received for 'Home' page")
+    return "Welcome to the page about Hawaii"
+
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# @app.route("/api/v1.0/names")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all passenger names"""
+#     # Query all passengers
+#     results = session.query(Passenger.name).all()
+
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
+
+#     return jsonify(all_names)
+
+
+# @app.route("/api/v1.0/passengers")
+# def passengers():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of passenger data including the name, age, and sex of each passenger"""
+#     # Query all passengers
+#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+#     session.close()
+
+#     # Create a dictionary from the row data and append to a list of all_passengers
+#     all_passengers = []
+#     for name, age, sex in results:
+#         passenger_dict = {}
+#         passenger_dict["name"] = name
+#         passenger_dict["age"] = age
+#         passenger_dict["sex"] = sex
+#         all_passengers.append(passenger_dict)
+
+#     return jsonify(all_passengers)
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
